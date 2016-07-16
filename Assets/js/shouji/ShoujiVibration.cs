49ddbdb371a35cef4583a6c648d4cad0e870dd00
chenkaixiong <1056387521@qq.com>
2016-07-16 15:48:14 +0800

    7.16
    加入三个物体，椅子、手机和排球

---
+++ Assets/js/shouji/ShoujiVibration.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShoujiVibration : MonoBehaviour {
+
+    public AudioClip vibration;
+    public AudioSource source;
+    public Animation anim;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    public void Play()
+    {
+        source.clip = vibration;
+        source.Play();
+        anim.Play();
+    }
+}
