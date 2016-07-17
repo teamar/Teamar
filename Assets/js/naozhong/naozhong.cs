d8d50d55daf614cc47271ffdb9ecbedd320f496d
justice1201 <61865688@qq.com>
2016-07-17 14:32:09 +0800

    闹钟，斑马，垃圾桶
    闹钟，斑马，垃圾桶

---
+++ Assets/js/naozhong/naozhong.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class naozhong : MonoBehaviour {
+	private AudioSource naozhong1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseDown(){
+		Debug.Log ("11111");
+		naozhong1 = gameObject.GetComponent<AudioSource> ();
+		naozhong1.Play ();
+		Debug.Log ("22222");
+	}
+}
