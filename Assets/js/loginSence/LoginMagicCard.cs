4b60face91abd493bab9b1ff2506d949ec4a3dd2
xiaoxiongchen <1056387521@qq.com>
2016-07-22 15:50:42 +0800

    ...

--- Assets/js/loginSence/LoginMagicCard.cs
+++ Assets/js/loginSence/LoginMagicCard.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class LoginMagicCard : MonoBehaviour {
 
+    public GameObject firstPanel;
+    public GameObject loadingPanel;
+    public UISlider slider;
+    public UILabel label;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,19 +15,45 @@ public class LoginMagicCard : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-		if (Input.GetMouseButtonDown(0)) {
-			Debug.Log("dddddd");
-			Ray ray = UICamera.mainCamera.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;  
-			if (Physics.Raycast(ray, out hit)) {
-				Debug.Log("gggggggggggggggggggg");
-				if (hit.collider.gameObject.name == this.name) {
-					Debug.Log("ffffffffffff");
-					Application.LoadLevel("card");
-				}
-			}  
-		}
 	}
 
+    public void PressButton()
+    {
+        firstPanel.SetActive(false);
+        loadingPanel.SetActive(true);
+        StartCoroutine(StartLoadingScene());
+    }
+
+    IEnumerator StartLoadingScene()
+    {
+        int displayProgress = 0;
+        int toProgress = 0;
+        AsyncOperation op = SceneManager.LoadSceneAsync(1);
+        op.allowSceneActivation = false;
+        while (op.progress < 0.9f)
+        {
+            toProgress = (int)op.progress * 100;
+            while (displayProgress < toProgress)
+            {
+                ++displayProgress;
+                UpdatePrgress(displayProgress);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+
+        toProgress = 100;
+        while (displayProgress < toProgress)
+        {
+            ++displayProgress;
+            UpdatePrgress(displayProgress);
+            yield return new WaitForEndOfFrame();
+        }
+        op.allowSceneActivation = true;
+    }
+
+    private void UpdatePrgress(int progress)
+    {
+        slider.value = progress / 100.0f;
+        label.text = progress + "%";
+    }
 }
